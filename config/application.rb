11a1e5465cee7ee6218e91d7e3b4154520a8d0f9
Semyz Serhii <semizserhii@gmail.com>
2019-03-03 19:18:18 +0200

    add active admin and fix user password

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -38,6 +38,10 @@ module AutoPartsStore
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Flash
 
   end
 end
