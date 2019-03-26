c661055948be40075e1de60f6d05e7523517fe61
Semyz Serhii <semizserhii@gmail.com>
2019-03-26 22:16:11 +0200

    remove uncomplited rspec tests
    refactoring for cors

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  namespace :api, defaults: {format: :json}  do
+  namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       resource :users, only: %i[create update show]
       resource :sessions, only: :create
