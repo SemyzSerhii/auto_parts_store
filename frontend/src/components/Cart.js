5373e3663cdf907521320b215063615c233675df
marina8915 <nonalatus@ukr.net>
2019-04-03 22:09:40 +0300

    added changeQuantity

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -19,10 +19,22 @@ class Cart extends Component {
         }
         this.deleteProduct = this.deleteProduct.bind(this)
         this.cleanCart = this.cleanCart.bind(this)
+        this.changeQuantity = this.changeQuantity.bind(this)
     }
 
     deleteProduct(id) {
         API.delete(`line_items/${id}`)
+            .then(function () {
+                window.location.reload()
+            })
+    }
+
+    changeQuantity(id, quantity, change) {
+        let new_quantity = change ? quantity + 1 : quantity - 1
+        API.put(`line_items/${id}`,
+            {
+                "quantity": new_quantity
+            })
             .then(function () {
                 window.location.reload()
             })
@@ -50,10 +62,10 @@ class Cart extends Component {
                     this.setState({ responseStatus: 'null' })
                 }
             }.bind(this), function () {
-                    this.setState({
-                        cart: [],
-                        responseStatus: 'null'
-                    })
+                this.setState({
+                    cart: [],
+                    responseStatus: 'null'
+                })
             }.bind(this))
     }
 
@@ -71,58 +83,85 @@ class Cart extends Component {
                             return <div className='cart'><ReactTable
                                 data={this.state.cart}
                                 columns={[
-                                        {
-                                            Header: 'Назва',
-                                            accessor: 'product',
-                                            Cell: row => (
-                                                <button
-                                                    className='nav-link btn btn-link'
-                                                    onClick={() => this.props.history.push(
-                                                        `/products/${row.value.id}`
-                                                    )}>{row.value.name}</button>
-                                            )
+                                    {
+                                        Header: 'Назва',
+                                        accessor: 'product',
+                                        Cell: row => (
+                                            <button
+                                                className='nav-link btn btn-link'
+                                                onClick={() => this.props.history.push(
+                                                    `/products/${row.value.id}`
+                                                )}>{row.value.name}</button>
+                                        )
 
-                                        },
-                                        {
-                                            Header: 'Image',
-                                            accessor: 'product',
-                                            Cell: row => (
-                                                <img
-                                                    width='75px'
-                                                    // check if image not -> visible standard image
-                                                    src={row.value.image.url ? row.value.image.url : noPhoto}
-                                                    alt={row.value.name}
-                                                />
-                                            )
-                                        },
-                                        {
-                                            Header: 'Кількість',
-                                            accessor: 'quantity'
-                                        },
-                                        {
-                                            Header: 'Ціна',
-                                            accessor: 'product.price'
-                                        },
-                                        {
-                                            Header: 'Разом',
-                                            accessor: 'product',
-                                            Cell: row => (row.original.quantity * row.value.price)
-                                        },
-                                        {
-                                            Header: '',
-                                            Cell: row => (
-                                                <button className='btn btn-primary'
-                                                   onClick={() => {
-                                                       this.deleteProduct(row.original.id)
-                                                   }
-                                                   }>
-                                                    Удалить
-                                                </button>
-                                            ),
-                                        }
+                                    },
+                                    {
+                                        Header: 'Image',
+                                        accessor: 'product',
+                                        Cell: row => (
+                                            <img
+                                                width='75px'
+                                                // check if image not -> visible standard image
+                                                src={row.value.image.url ? row.value.image.url : noPhoto}
+                                                alt={row.value.name}
+                                            />
+                                        )
+                                    },
+                                    {
+                                        Header: '',
+                                        width: 50,
+                                        Cell: row => (
+                                            <button className='btn btn-link'
+                                                    onClick={() => {
+                                                        this.changeQuantity(row.original.id, row.original.quantity, true)
+                                                    }
+                                                    }>
+                                                <i className="fa fa-plus"></i>
+                                            </button>
+                                        ),
+                                    },
+                                    {
+                                        Header: 'Кількість',
+                                        accessor: 'quantity'
+                                    },
+                                    {
+                                        Header: '',
+                                        width: 50,
+                                        Cell: row => (
+                                            <button className='btn btn-link'
+                                                    onClick={() => {
+                                                        this.changeQuantity(row.original.id, row.original.quantity, false)
+                                                    }
+                                                    }>
+                                                <i className="fa fa-minus"></i>
+                                            </button>
+                                        ),
+                                    },
+                                    {
+                                        Header: 'Ціна',
+                                        accessor: 'product.price'
+                                    },
+                                    {
+                                        Header: 'Разом',
+                                        accessor: 'product',
+                                        Cell: row => (row.original.quantity * row.value.price)
+                                    },
+                                    {
+                                        Header: '',
+                                        width: 50,
+                                        Cell: row => (
+                                            <button className='btn btn-link'
+                                                    onClick={() => {
+                                                        this.deleteProduct(row.original.id)
+                                                    }
+                                                    }>
+                                                <i className="fa fa-times"></i>
+                                            </button>
+                                        ),
+                                    }
                                 ]}
-                        pageSize={(items < 10) ? items : 10}
-                        className='-striped -highlight'
+                                pageSize={(items < 10) ? items : 10}
+                                className='-striped -highlight'
                             />
                                 {this.state.cart.length > 0 ?
                                     (<div><h5>Загальна вартість: {sum}</h5>
